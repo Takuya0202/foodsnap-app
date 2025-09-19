208dcd5a323db16710db1a855ccb550f1bc8d168
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2025-09-20 07:22:02 +0900

    honoRPCに対応させるため、ディレクトリ構成を変更

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,8 @@
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import { createUserSchema } from "../schema/user";
+
+const userApp = new Hono().basePath('/user')
+.post('/register' , zValidator( 'json' , createUserSchema) , async (c) => {
+    
+})
