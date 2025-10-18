227251b9e0ae469ada8d6b3756a48c07e87b8d39
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2025-10-18 18:58:37 +0900

    add

--- frontend/app/stores/top/page.tsx
+++ frontend/app/stores/top/page.tsx
@@ -1,4 +1,23 @@
+"use client"
+import { client } from "@/utils/setting";
+import { useEffect, useState } from "react";
+
+function TopSkeleton() {
+  return (
+    <>
+    </>
+  )
+}
+
+
 export default function TopPage() {
+  const [isLoading , setIsLoading] = useState<boolean>(false);
+  const [stores , setStores ] = useState([]);
+  const [position , setPosition] = useState<{latitude : number , longitude : number} | null>(null);
+
+  useEffect(() => {
+    const 
+  })
   return (
     <div>
       <h1>TopPage</h1>
